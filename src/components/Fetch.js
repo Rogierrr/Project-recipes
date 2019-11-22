4044db0c3263d83e26e6bdbf6aa06862c3ecd8e8
Rogier van Beers <rogierrr@hotmail.com>
2019-11-22 10:02:57 +0100

    changed and added some pics

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 
 class Fetch extends Component {
+  componentDidMount() {
+    // fetch("https://api.openbrewerydb.org/breweries")
+    fetch("http://www.recipepuppy.com/api/")
+      .then(res => res.json())
+      .then(json => {
+        console.log("json", json);
+      });
+  }
   render() {
+    console.log("fetch render");
     return <div>fetch</div>;
   }
 }
